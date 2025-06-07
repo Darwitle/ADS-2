e3f162ab6cc381f8cf35abcae661c20bef867a3c
Darwitle <margonesnanowa@gmail.com>
2025-06-07 21:00:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,43 @@
 
 
 double pown(double value, uint16_t n) {
-  return 0.0;
+  if (n == 0) return 1;
+  return value * pown(value, n-1);
 }
 
 uint64_t fact(uint16_t n) {
-  return 0.0;
+  if (n == 0 || n == 1) return 1;
+  return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-  return 0.0;
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-  return 0.0;
+  double dres = 1;
+  for (int i = 1; i <= count; i++) {
+      dres += calcItem(x, i);
+  }
+  return dres;
 }
 
 double sinn(double x, uint16_t count) {
-  return 0.0;
+  double dres = x;
+  double dsign = 1;
+  for (int i = 3; i <= (2 * count - 1); i = i + 2) {
+    dsign *= -1;
+    dres += calcItem(x, i) * dsign;
+  }
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-  return 0.0;
+  double dres = 1;
+  double dsign = 1;
+  for (int i = 2; i <= (2 * count - 2); i = i + 2) {
+    dsign *= -1;
+    dres += calcItem(x, i) * dsign;
+  }
+  return dres;
 }
